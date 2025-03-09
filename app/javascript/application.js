6f5b8b0de5dcbc0333f99b013cdbef1e2acf16e5
Jeremy Walton <jeremy.patrick.walton@gmail.com>
2025-03-09 16:59:50 -0400

    Run modals generator

--- app/javascript/application.js
+++ app/javascript/application.js
@@ -1,3 +1,6 @@
 // Configure your import map in config/importmap.rb. Read more: https://github.com/rails/importmap-rails
 import "@hotwired/turbo-rails"
 import "controllers"
+import './initializers/turbo_confirm.js'
+import './initializers/frame_missing_handler.js'
+import './initializers/before_morph_handler.js'
