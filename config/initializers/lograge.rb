c1a33e3d79b72de78361e2b91172eaa4adcc05a0
Jeremy Walton <jeremy.patrick.walton@gmail.com>
2025-03-10 22:44:04 -0400

    Disable lograge

--- config/initializers/lograge.rb
+++ config/initializers/lograge.rb
@@ -1,16 +1,17 @@
 # frozen_string_literal: true
 
+# TODO: re-enable lograge after Devise is installed
 Rails.application.configure do
-  config.lograge.enabled = true
-  config.lograge.custom_payload do |controller|
-    {
-      user_id: controller.current_user.try(:id)
-    }
-  end
-  config.lograge.custom_options = lambda do |event|
-    exceptions = %w[controller action format id]
-    {
-      params: event.payload[:params].except(*exceptions)
-    }
-  end
+  # config.lograge.enabled = true
+  # config.lograge.custom_payload do |controller|
+  #   {
+  #     user_id: controller.current_user.try(:id)
+  #   }
+  # end
+  # config.lograge.custom_options = lambda do |event|
+  #   exceptions = %w[controller action format id]
+  #   {
+  #     params: event.payload[:params].except(*exceptions)
+  #   }
+  # end
 end
