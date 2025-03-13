e282ecfca53e711907ee1c736c32c0047fdc19f1
Jeremy Walton <jeremy.patrick.walton@gmail.com>
2025-03-12 20:41:47 -0400

    remove unused input

--- app/inputs/grouped_collection_select_input.rb
+++
@@ -1,7 +0,0 @@
-# frozen_string_literal: true
-
-class GroupedCollectionSelectInput < SimpleForm::Inputs::GroupedCollectionSelectInput
-  def input_html_classes
-    super.push('form__dropdown')
-  end
-end
