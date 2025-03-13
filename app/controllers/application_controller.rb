d43305fd969f4e680129a5db125ea799ec3b5862
Jeremy Walton <jeremy.patrick.walton@gmail.com>
2025-03-12 20:56:24 -0400

    Remove option (#10)
    This PR removes the allow browser option

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -2,5 +2,6 @@
 
 class ApplicationController < ActionController::Base
   # Only allow modern browsers supporting webp images, web push, badges, import maps, CSS nesting, and CSS :has.
-  allow_browser versions: :modern
+  # Disabled for now so I can do proper lighthouse testing
+  # allow_browser versions: :modern
 end
