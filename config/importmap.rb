8e30c410c66dae07d3d4e5e4fd6e50ecf0ffff9c
Jeremy Walton <jeremy.patrick.walton@gmail.com>
2025-03-15 16:23:43 -0400

    Add shoelace pin

--- config/importmap.rb
+++ config/importmap.rb
@@ -5,6 +5,7 @@ pin "@hotwired/turbo-rails", to: "turbo.min.js"
 pin "@hotwired/stimulus", to: "stimulus.min.js"
 pin "@hotwired/stimulus-loading", to: "stimulus-loading.js"
 pin "@rolemodel/turbo-confirm", to: "@rolemodel--turbo-confirm.js" # @2.1.1
+pin '@shoelace-style/shoelace/components/rating', to: 'https://cdn.jsdelivr.net/npm/@shoelace-style/shoelace@2.20.1/cdn/components/rating/rating.js' # @2.20.1
 
 pin_all_from "app/javascript/initializers", under: "initializers"
 pin_all_from "app/javascript/controllers", under: "controllers"
