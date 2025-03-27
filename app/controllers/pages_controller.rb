14449dc4718768ab0ebe2c7771663aa9746a7d1a
Jeremy Walton <jeremy.patrick.walton@gmail.com>
2025-03-26 23:00:05 -0400

    Set the root path back

--- app/controllers/pages_controller.rb
+++ app/controllers/pages_controller.rb
@@ -4,6 +4,6 @@ class PagesController < ApplicationController
   skip_before_action :authenticate_user!, only: :index
 
   def index
-    redirect_to root_path if current_user
+    redirect_to brews_path if current_user
   end
 end
